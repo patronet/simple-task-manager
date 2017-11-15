851702f8f7f62193735de176ff4e85f8c63e82fb
horvath <horvath@patronet.net>
2017-11-15 15:34:35 +0100

    Merge commit '02e0ae33d7470618133b0d4e79cde5ceab1e71ab'

--- autoload.php
+++ autoload.php
@@ -11,9 +11,5 @@ require_once(__DIR__ . '/PatroNet/Core/Autoload/PathAutoloader.php');
 require_once(__DIR__ . '/PatroNet/Core/Autoload/RegexPathAutoloader.php'); // XXX
 require_once(__DIR__ . '/PatroNet/Core/Autoload/Registrator.php');
 
-<<<<<<< HEAD
 Registrator::register(new PathAutoloader('PatroNet\\Core', __DIR__ . '/PatroNet/Core'));
-=======
-Registrator::register(new Psr0Autoloader('PatroNet\\Core', __DIR__ . '/PatroNet/Core'));
->>>>>>> db192bd004b3e9f254c1ba834db6307aed187532
 
